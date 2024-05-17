dd6a216ae203cee466c72d8a32f58140b2aec4e1
Gracella Noveliora <noveliora.gracella@gmail.com>
2024-05-17 23:49:04 +0700

    Initial Commit

---
+++ ToneSync/ToneSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneSyncApp.swift
+//  ToneSync
+//
+//  Created by Gracella Noveliora on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ToneSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
